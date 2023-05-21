87eab61dafa966d8ec8710c3a3a58ace1893d27b
KseniiaBryzhko <kseniya.brizhko@gmail.com>
2023-05-21 17:21:25 +0300

    app was refactored to hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,3 @@
-import { Component } from 'react';
 import { Searchbar } from './Searchbar/Searchbar';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import { Loader } from './Loader/Loader';
@@ -6,88 +5,76 @@ import { Button } from './Button/Button';
 import { ToastContainer } from 'react-toastify';
 import * as ImageService from 'service/image-service';
 import css from './App.module.css';
+import { useState, useEffect } from 'react';
 
-export class App extends Component {
-  state = {
-    query: '',
-    page: 1,
-    images: [],
-    perPage: 12,
-    totalResults: 0,
-    error: null,
-    showGallery: false,
-    isLoading: false,
-  };
+export const App = () => {
+  const [query, setQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [images, setImages] = useState([]);
+  const [perPage, setPerPage] = useState(12);
+  const [totalResults, setTotalResults] = useState(0);
+  const [error, setError] = useState(null);
+  const [showGallery, setShowGallery] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [prevQuery, setPrevQuery] = useState('');
+  const [prevPage, setPrevPage] = useState(1);
 
-  componentDidUpdate(prevProps, prevState) {
-    const { query, page } = this.state;
-    if (prevState.query !== query || prevState.page !== page) {
-      this.getPhotos(query, page);
+  useEffect(() => {
+    if (prevQuery !== query || prevPage !== page) {
+      getPhotos(query, page);
     }
-  }
+  }, [query, page, prevPage, prevQuery]);
 
-  handleSubmit = data => {
-    this.setState({
-      query: data,
-      page: 1,
-      images: [],
-      perPage: 12,
-      totalResults: 0,
-      error: null,
-      showGallery: false,
-      isLoading: false,
-    });
+  const handleSubmit = data => {
+    setQuery(data);
+    setPage(1);
+    setImages([]);
+    setPerPage(12);
+    setTotalResults(0);
+    setError(null);
+    setShowGallery(false);
+    setIsLoading(false);
   };
 
-  getPhotos = async (query, page) => {
-    this.setState({ isLoading: true });
+  const getPhotos = async (query, page) => {
+    setIsLoading(true);
     try {
       const { hits, totalHits } = await ImageService.getImages(query, page);
-      this.setState(prevState => ({
-        images: [...prevState.images, ...hits],
-        totalResults: totalHits,
-      }));
+
+      setImages(prevImages => [...prevImages, ...hits]);
+      setTotalResults(totalHits);
+
       if (hits.length === 0) {
-        this.setState({
-          showGallery: true,
-        });
+        setShowGallery(true);
       }
     } catch (error) {
-      this.setState({
-        error: error.message,
-      });
-      console.log(error);
+      setError(error.message);
     } finally {
-      this.setState({ isLoading: false });
+      setIsLoading(false);
     }
   };
 
-  handleLoadMore = () => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
-    }));
+  const handleLoadMore = () => {
+    setPage(prevPage => prevPage + 1);
   };
 
-  render() {
-    const { error, isLoading, images, totalResults, showGallery } = this.state;
-    return (
-      <div className={css.App}>
-        <Searchbar onSubmit={this.handleSubmit} />
-        {showGallery && (
-          <p className={css.Text}>Sorry, there is no images for your query</p>
-        )}
-        {error && (
-          <p className={css.Text}>
-            Whoops, something went wrong: {error.message}
-          </p>
-        )}
-        {isLoading && <Loader />}
-        {images.length > 0 && <ImageGallery images={images} />}
-        {images.length > 0 && images.length < totalResults && (
-          <Button onClick={this.handleLoadMore} textChange={isLoading}></Button>
-        )}
-        <ToastContainer autoClose={2000} />
-      </div>
-    );
-  }
-}
+  return (
+    <div className={css.App}>
+      <Searchbar onSubmit={handleSubmit} />
+      {showGallery && (
+        <p className={css.Text}>Sorry, there is no images for your query</p>
+      )}
+      {error && (
+        <p className={css.Text}>
+          Whoops, something went wrong: {error.message}
+        </p>
+      )}
+      {isLoading && <Loader />}
+      {images.length > 0 && <ImageGallery images={images} />}
+      {images.length > 0 && images.length < totalResults && (
+        <Button onClick={handleLoadMore} textChange={isLoading}></Button>
+      )}
+      <ToastContainer autoClose={2000} />
+    </div>
+  );
+};
